7ac12cbd06e3be4c46212a87a09c976c14d3ff19
AdamNowak02 <adam.nowak@microsoft.wsei.edu.pl>
2024-12-17 13:08:05 +0100

    dodanie hostingu

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'simongameapp',
+  location: 'us-central1'
+};
+
